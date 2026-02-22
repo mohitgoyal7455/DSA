3cdfd58257862d60d218c7758b33a4a67ab98f40
MOHIT GOYAL <168035609+mohitgoyal7455@users.noreply.github.com>
2026-02-22 19:47:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (94.66%) - LeetSync

--- 899-binary-gap/binary-gap.cpp
+++ 899-binary-gap/binary-gap.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int binaryGap(int n) {
-        string s = bitset<32>(n).to_string();
-        s = s.substr(s.find('1'));
 
-        int i = 0, j = 0;
-        int x = s.length(), maxi = 0;
+        int prev = -1;
+        int i = 0;
+        int maxlen = 0;
 
-        while (j < x && i < x) {
-            if (s[i] == '1') {
-                j = i + 1;
-                while (j < x && s[j] != '1') {
-                    j++;
+        while(n) {
+            if (n & 1) {
+                if (prev != -1) {
+                    maxlen = max(maxlen, i - prev);
                 }
-                if (j < x && s[j] == '1')
-                    maxi = max(maxi, j - i);
+                prev = i;
             }
+            n >>=1;
             i++;
         }
-        return maxi;
+        
+        return maxlen;
     }
 };

f5d4626fced521fee3e6b2513c2409b7141f6c50
MOHIT GOYAL <168035609+mohitgoyal7455@users.noreply.github.com>
2026-03-09 07:34:18 +0530

    Time: 156 ms (75.61%) | Memory: 17.4 MB (52.03%) - LeetSync

---
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numberOfStableArrays(int zero, int one, int limit) {
+        const int mod = 1'000'000'007;
+
+        vector<vector<int>> dp0(zero + 1, vector<int>(one + 1, 0)); 
+        vector<vector<int>> dp1(zero + 1, vector<int>(one + 1, 0)); 
+
+        for (int i = 1; i <= min(zero, limit); ++i) dp0[i][0] = 1;
+        for (int j = 1; j <= min(one, limit); ++j) dp1[0][j] = 1;
+
+    
+        for (int i = 1; i <= zero; ++i) {
+            for (int j = 1; j <= one; ++j) {
+                for (int k = 1; k <= min(limit, i); ++k)
+                    dp0[i][j] = (dp0[i][j] + dp1[i - k][j]) % mod;
+                for (int k = 1; k <= min(limit, j); ++k)
+                    dp1[i][j] = (dp1[i][j] + dp0[i][j - k]) % mod;
+            }
+        }
+
+        return (dp0[zero][one] + dp1[zero][one]) % mod;
+    }
+};

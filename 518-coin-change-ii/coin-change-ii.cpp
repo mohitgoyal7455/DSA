c3e5eebbee642d790348184ddf41f081d5ba07c6
MOHIT GOYAL <168035609+mohitgoyal7455@users.noreply.github.com>
2026-03-29 05:07:55 +0530

    Time: 99 ms (6.08%) | Memory: 16.7 MB (65.70%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int n;
+    int t[301][5001];
+    int solve(int i,int amount, vector<int>& coins){
+        if(amount==0){
+            return 1;
+        }
+        if(i==n){
+            return 0;
+        }
+        if(amount<coins[i]){
+            return solve(i+1,amount,coins);
+        }
+        if(t[i][amount]!=-1){return t[i][amount];}
+        
+        int take=solve(i,amount-coins[i],coins);
+        int skip=solve(i+1,amount,coins);
+        return t[i][amount]=take+skip;
+    }
+    int change(int amount, vector<int>& coins) {
+        n=coins.size();
+        memset(t,-1,sizeof(t));
+        return solve(0,amount,coins);
+        
+    }
+};

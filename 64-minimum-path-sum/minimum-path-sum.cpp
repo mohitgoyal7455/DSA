7cef936ed50a950065c26d4dee8e490fe8c1d95a
MOHIT GOYAL <168035609+mohitgoyal7455@users.noreply.github.com>
2026-03-29 00:50:59 +0530

    Time: 3 ms (29.34%) | Memory: 15.6 MB (76.93%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-   int t[201][201];
-  
+    int t[201][201];
     int solve(int i,int j,int n,int m,vector<vector<int>>& grid){
-        if(i<0||j<0||i>=n||j>=m){
-            return INT_MAX;
+        if(i<0||i>n||j<0 ||j>m){
+            return 0;
         }
         if(t[i][j]!=-1){
             return t[i][j];
         }
         if(i==n-1 && j==m-1){
-          
-           
             return t[i][j]=grid[i][j];
         }
-         
-        int right=solve(i,j+1,n,m,grid);
-        int down=solve(i+1,j,n,m,grid);
-        
-        return t[i][j]=grid[i][j]+min(right,down);
+        else if(i==n-1){
+            return t[i][j]=grid[i][j]+solve(i,j+1,n,m,grid);
+        }
+        else if(j==m-1){
+            return t[i][j]=grid[i][j]+solve(i+1,j,n,m,grid);
+        }
+        else{
+            return t[i][j]=grid[i][j]+min(solve(i,j+1,n,m,grid),solve(i+1,j,n,m,grid));
+        }
     }
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
         memset(t,-1,sizeof(t));
         return solve(0,0,n,m,grid);
+        
+
         
     }
 };

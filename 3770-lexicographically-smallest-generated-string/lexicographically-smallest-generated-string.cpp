66abdc092a7aae449674205be070d174d0edf303
MOHIT GOYAL <168035609+mohitgoyal7455@users.noreply.github.com>
2026-03-31 13:39:17 +0530

    Time: 17 ms (61.02%) | Memory: 11.3 MB (45.76%) - LeetSync

--- 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.cpp
+++ 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.cpp
@@ -1,43 +1,72 @@
 class Solution {
 public:
+    bool isSame(string &word, string & str2, int i, int m) {
+        for(int j = 0; j < m; j++) {
+            if(word[i] != str2[j]) {
+                return false;
+            }
+
+            i++;
+        }
+
+        return true;
+    }
+
     string generateString(string str1, string str2) {
-        int n = str1.size(), m = str2.size();
-        string s(n + m - 1, 'a');
-        vector<int> fixed(n + m - 1, 0);
-        for (int i = 0; i < n; i++) {
-            if (str1[i] == 'T') {
-                for (int j = i; j < i + m; j++) {
-                    if (fixed[j] && s[j] != str2[j - i]) {
+        int n = str1.length();
+        int m = str2.length();
+
+        int N = n + m - 1;
+        string word(N, '$');
+
+        vector<bool> canChange(N, false);
+
+
+        for(int i = 0; i < n; i++) {
+            if(str1[i] == 'T') {
+                int i_ = i;
+                for(int j = 0; j < m; j++) {
+
+                   if(word[i_] != '$' && word[i_] != str2[j]) {
                         return "";
-                    } else {
-                        s[j] = str2[j - i];
-                        fixed[j] = 1;
-                    }
+                   }
+                   word[i_] = str2[j];
+                   i_++;
                 }
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            if (str1[i] == 'F') {
-                bool flag = false;
-                int idx = -1;
-                for (int j = i + m - 1; j >= i; j--) {
-                    if (str2[j - i] != s[j]) {
-                        flag = true;
-                    }
-                    if (idx == -1 && !fixed[j]) {
-                        idx = j;
-                    }
-                }
-                if (flag) {
-                    continue;
-                } else if (idx != -1) {
-                    s[idx] = 'b';
-                } else {
-                    return "";
-                }
+        
+        for(int i = 0; i < N; i++) {
+            if(word[i] == '$') {
+                word[i] = 'a';
+                canChange[i] = true;
             }
         }
-        return s;
+
+        for(int i = 0; i < n; i++) {
+            if(str1[i] == 'F') {
+
+                if(isSame(word, str2, i, m)) { 
+                    
+                    bool changed = false;
+                    for(int k = i + m - 1; k >= i; k--) {
+                        if(canChange[k] == true) {
+                            word[k] = 'b';
+                            changed = true;
+                            break;
+                        }
+                    }
+
+                    if(changed == false) { 
+                        return "";
+                    }
+
+                }
+
+            }
+        }
+
+        return word;
     }
 };

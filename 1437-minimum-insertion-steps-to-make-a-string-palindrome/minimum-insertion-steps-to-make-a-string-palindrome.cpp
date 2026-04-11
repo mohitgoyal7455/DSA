5dc088aedce4933bc018f8c36fb0ea9ef2229534
MOHIT GOYAL <168035609+mohitgoyal7455@users.noreply.github.com>
2026-04-11 21:44:49 +0530

    Time: 71 ms (6.76%) | Memory: 31.6 MB (38.95%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    int t[501][501];
-    int check(string &s,int i,int j){
-            
-            if(i>=j){
-                return 0;
-            }
-            if(t[i][j]!=-1){
-                return t[i][j];
-            }
-            if(s[i]==s[j]){
-                return t[i][j]=check(s,i+1,j-1);
-            }
-           return t[i][j]=1+min(check(s,i+1,j),check(s,i,j-1));
-    }
-    int minInsertions(string s) {
-        int n=s.length();
-        memset(t,-1,sizeof(t));
-        return check(s,0,n-1);
+    int solve(string& s,string& t,int i,int j,int m,int n,vector<vector<int>>& dp){
+        if(i == m || j == n) return 0;
 
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i] == t[j]){
+            dp[i][j] = 1 + solve(s,t,i+1,j+1,m,n,dp);
+        }else{
+            dp[i][j] = max(solve(s,t,i+1,j,m,n,dp),solve(s,t,i,j+1,m,n,dp));
+        }
+
+        return dp[i][j];
+    }
+
+    int minInsertions(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
         
+        int m = s.length();
+        int n = t.length();
+
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+
+        int ans = solve(s,t,0,0,m,n,dp);
+
+        return m - ans;
     }
 };

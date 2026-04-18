150ecbacee13a8f85cb1eb34cd0e0f245f083000
MOHIT GOYAL <168035609+mohitgoyal7455@users.noreply.github.com>
2026-04-19 03:26:22 +0530

    Time: 9 ms (5.22%) | Memory: 16.4 MB (14.97%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    int t[201][201];
-    int solve(int i,int j,int n,int m,vector<vector<int>>& grid){
-        if(i<0||i>n||j<0 ||j>m){
-            return 0;
-        }
-        if(t[i][j]!=-1){
-            return t[i][j];
-        }
-        if(i==n-1 && j==m-1){
-            return t[i][j]=grid[i][j];
-        }
-        else if(i==n-1){
-            return t[i][j]=grid[i][j]+solve(i,j+1,n,m,grid);
-        }
-        else if(j==m-1){
-            return t[i][j]=grid[i][j]+solve(i+1,j,n,m,grid);
-        }
-        else{
-            return t[i][j]=grid[i][j]+min(solve(i,j+1,n,m,grid),solve(i+1,j,n,m,grid));
-        }
-    }
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        memset(t,-1,sizeof(t));
-        return solve(0,0,n,m,grid);
-        
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
 
-        
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
+
+        pq.push({grid[0][0],{0,0}});
+        dist[0][0]=grid[0][0];
+        int rows[2]={1,0};
+        int cols[2]={0,1};
+        while(!pq.empty())
+        {
+            auto it=pq.top();
+            pq.pop();
+            int r=it.second.first;
+            int c=it.second.second;
+            int sum=it.first;
+            for(int i=0;i<2;i++)
+            {
+                int nrow=r+rows[i];
+                int ncol=c+cols[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && dist[nrow][ncol]>sum+grid[nrow][ncol])
+                {
+                    pq.push({sum+grid[nrow][ncol],{nrow,ncol}});
+                    dist[nrow][ncol]=sum+grid[nrow][ncol];
+                }
+            }
+        }
+        return dist[n-1][m-1];
+
     }
 };

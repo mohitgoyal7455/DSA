3db8df6b84e0617623e2239e29fe6f5b5099222d
MOHIT GOYAL <168035609+mohitgoyal7455@users.noreply.github.com>
2026-07-10 02:26:18 +0530

    Time: 3 ms (29.33%) | Memory: 8.6 MB (20.96%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int reverse(int x) {
-        bool neg = x < 0;
+    long long reverse(int x) {
+        long long ans=0;
+        while(x!=0){
+            int digit=x%10;
 
-        string s = to_string(abs((long long)x));
-        ::reverse(s.begin(), s.end());
-
-        long long ans = stoll(s);
-
-        if (neg) ans = -ans;
-
-        return (ans > INT_MAX || ans < INT_MIN) ? 0 : ans;
+            ans=ans*10+digit;
+            if(ans>INT_MAX || ans<INT_MIN){
+                return 0;
+            }
+            x=x/10;
+        }
+        return ans;
     }
 };

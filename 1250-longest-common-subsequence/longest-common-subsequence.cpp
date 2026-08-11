581c35a5965a302552e87e31c8f335b09ec5f787
MOHIT GOYAL <168035609+mohitgoyal7455@users.noreply.github.com>
2026-08-11 20:52:19 +0530

    Time: 32 ms (36.47%) | Memory: 13 MB (83.88%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -3,16 +3,16 @@ class Solution {
 public:
     int t[1001][1001];
     int LCS(string& s1, string& s2, int m, int n) {
-        if(m == 0 || n == 0)
+        if(m>=s1.length() || n>=s2.length())
             return t[m][n] = 0;
         
         if(t[m][n] != -1)
             return t[m][n];
         
-        if(s1[m-1] == s2[n-1])
-            return t[m][n] = 1 + LCS(s1, s2, m-1, n-1);
+        if(s1[m] == s2[n])
+            return t[m][n] = 1 + LCS(s1, s2, m+1, n+1);
         
-        return t[m][n] = max(LCS(s1, s2, m, n-1), LCS(s1, s2, m-1, n));
+        return t[m][n] = max(LCS(s1, s2, m, n+1), LCS(s1, s2, m+1, n));
     }
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.length();
@@ -20,6 +20,6 @@ public:
         
         memset(t, -1, sizeof(t));
         
-        return LCS(text1, text2, m, n);
+        return LCS(text1, text2, 0, 0);
     }
 };
